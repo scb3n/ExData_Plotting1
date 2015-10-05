6773ee1e2190867d68548cb1736b8a44519c56a5
scb3n <sblanch@gmail.com>
2015-10-05 13:30:16 -0400

    script and output for plot 3

---
+++ plot3.R
@@ -0,0 +1,20 @@
+# read the data file (assume the file has been extracted to the working directory)
+data <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", strip.white = TRUE, stringsAsFactors = FALSE)
+
+# convert dates
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# keep only dates that we are going to plot (2007-02-01 and 2007-02-02)
+data <- filter(data, Date >= as.Date("2007-02-01", format = "%Y-%m-%d"), Date <= as.Date("2007-02-02", format = "%Y-%m-%d"))
+
+# Group the date/time fields into a single field in a new column
+data$DateTime <- strptime(paste(data$Date, data$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# make the plot
+png("plot3.png", width = 480, height = 480, units = "px")
+plot(data$DateTime, data$Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab = "")
+lines(data$DateTime, data$Sub_metering_1, col = "black")
+lines(data$DateTime, data$Sub_metering_2, col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+legend("topright", lwd = 1, legend = colnames(data[,7:9]), col = c("black", "red", "blue"))
+dev.off()
